cc17ef51709cc721c369f6e6f63af4c2ed352e1b
weider86 <weider.lima@mercadolivre.com>
2021-10-04 16:30:34 -0300

    add react and folder strutcture initial implementations

---
+++ src/client/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './index.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Learning React and GraphQl</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
